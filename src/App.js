2335b8e91802e7e20ab8a14cd136cb3c7936fffd
Rishabh Agnihotri <rishabh.agnihotri@beamstacks.com>
2023-12-30 21:51:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './Login';
+import Home from './Home';
+import Cart from './component/Cart'; // Import the Cart component
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/cart" element={<Cart />} /> {/* Add a route for the Cart component */}
+        
+        <Route path="/" element={<Navigate to="/login" />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
